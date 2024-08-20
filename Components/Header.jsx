30dff0642949363d0465aa5ff6a3dbb3cc603eed
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-08-20 22:35:17 +0530

    Header Buttons

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -5,9 +5,20 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='py-5 px-5 md:px-12 lg:px-28'>
-      <div className='bg-blue flex justify-between items-center'>
+      <div className=' flex justify-between items-center'>
         <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto'/>
+        <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000] '>Get Started  <Image src={assets.arrow}/> </button>
       </div>
+
+    <div className='text-center my-8'>
+       <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blogs</h1>
+       <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque vel pariatur, facilis dolorem ratione asperiores nostrum soluta nobis cumque quibusdam nemo laborum aut sint, sequi laboriosam amet illo iste sapiente. Pariatur inventore cupiditate deleniti ullam optio dolores ea consequatur ipsa?</p> 
+
+       <form action="" className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]'  >
+        <input type="email"  placeholder='Enter Your Email' className='pl-4 outline-none'/>
+       <button className='border-l border-black py-4 sm:px-8 active:bg-gray-600 active:text-white' type='submit'>Subscribe</button>
+       </form>
+    </div>  
     </div>
   )
 }
