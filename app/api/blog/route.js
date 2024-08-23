2b894a5be36a18e8a91297f07792a0f3f9326c02
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-08-23 12:14:41 +0530

    blog posts backend code. router.js

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,7 +1,9 @@
 import { ConnectDB } from "@/lib/config/db"
+import BlogModel from "@/lib/models/BlogModel";
 
 const { NextResponse } = require("next/server")
 import { writeFile } from 'fs/promises'
+import { title } from "process";
 
 const LoadDB = async () => {
     await ConnectDB();
@@ -25,5 +27,18 @@ export async function POST(req) {
     await writeFile(path, buffer);
     const imgUrl = `/${timestamp}_${image.name}`;
     console.log(imgUrl);
-    return NextResponse.json({ imgUrl });
+
+        const blogData={
+            title:`${formData.get('title')}`,
+            description:`${formData.get('description')}`,
+            category:`${formData.get('category')}`,
+            author:`${formData.get('author')}`,
+            image:`${imgUrl}`,
+            authorImg:`${formData.get('authorImg')}`
+        }
+
+        await BlogModel.create(blogData);
+        console.log("Blog Saved");
+
+    return NextResponse.json({ success:true,msg:"Blog Added" });
 }
