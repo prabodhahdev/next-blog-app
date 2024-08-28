d569d44678490e95591a0c0f7647f6c047ee68e1
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-08-28 20:03:23 +0530

    added api for add blog post and added toastify for layout

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -1,14 +1,50 @@
 'use client'
 import { assets } from '@/Assets/assets'
+import axios from 'axios'
 import Image from 'next/image'
 import React, { useState } from 'react'
+import { toast } from 'react-toastify'
 
 const Page = () => {
   const [image, setImage] = useState(false);
+  const [data,setData]=useState({
+    title:"",
+    description:"",
+    category:"Startup",
+    author:"Prabodha",
+    authorImg:"/authorImg.png"
+  })
+
+const onChangeHandler=(event)=>{
+  const name=event.target.name;
+  const value=event.target.value;
+  setData(data=>({...data,[name]:value}));
+  console.log(data);
+}
+
+const onSubmitHandler =async(e)=>{
+  e.preventDefault();
+  const formData= new formData();
+  formData.append('title',data.title);
+  formData.append('description',data.description);
+  formData.append('category',data.category);
+  formData.append('author',data.author);
+  formData.append('authorImg',data.authorImg);
+  formData.append('image',data.image);
+
+  const response=await axios.post('/api/blog',formData);
+  if(response.data.success){
+    toast.success(response.data.msg);
+  }
+  else{
+    toast.error("Error");
+  }
+
+}
 
   return (
     <>
-      <form className='pt-5 px-5 sm:pt-12 sm:pl-16'>
+      <form onSubmit={onChangeHandler} className='pt-5 px-5 sm:pt-12 sm:pl-16'>
         <p className='text-xl'>Upload thumbnail</p>
         <label htmlFor="image">
           <Image className='mt-4' src={!image ? assets.upload_area : URL.createObjectURL(image)} width={140} height={70} alt='' />
@@ -16,12 +52,12 @@ const Page = () => {
         <input type="file" id='image' hidden required onChange={(e) => setImage(e.target.files[0])} />
       
       <p className='text-xl mt-4'>Blog Title</p>
-      <input className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='Type here' required />
+      <input name='title' onChange={onChangeHandler} value={data.title} className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='Type here' required />
 
       <p className='text-xl mt-4'>Blog Description</p>
-      <textarea className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='Write content  here' required />
+      <textarea name='description' onChange={onChangeHandler} value={data.description} className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='Write content  here' required />
       <p className='text-xl mt-4 '>Blog category</p>
-      <select name="category" className='w-40 mt-4 px-4 py-3 border text-gray-500 ' >
+      <select name="category" onChange={onChangeHandler} value={data.category} className='w-40 mt-4 px-4 py-3 border text-gray-500 ' >
         <option value="Startup">Startup</option>
         <option value="Technology">Technology</option>
         <option value="Lifestyle">Lifestyle</option>
