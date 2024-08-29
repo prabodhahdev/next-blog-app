92c1fbccb71a59a96ab5ec258e2b5ea54279ecf9
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-08-29 16:56:06 +0530

    global css

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -12,7 +12,8 @@ const BlogItem = ({ title, category, description, image,id }) => {
       <p className='ml-5 mt-5 px-0 inline-block bg-black text-white text-sm'>{category}</p>
       <div className="p-5">
         <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>{title}</h5>
-        <p className='mb-3 text-sm tracking-tight text-gray-700'>{description}</p>
+        <p className='mb-3 text-sm tracking-tight text-gray-700' dangerouslySetInnerHTML={{__html:description.slixe(0,120)}}>
+        </p>
         <Link href={`/blogs/${id}`} className='inline-flex items-center py-2 font-semibold text-center gap-2 '>
           Read more <Image src={assets.arrow} className='ml-2' width={12} height={12} />
         </Link>
--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -52,19 +52,10 @@ const Page = ({ params }) => {
 
       <div className="mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10">
         <Image className='border-4 border-white' src={data.image} width={1280} height={720} alt='Blog Image' />
-        <h1 className='my-8 text-[26px] font-semibold'>Introduction:</h1>
-        <p>{data.description}</p>
-        <h3 className='my-5 text-[18px] font-semibold'>Step 1: Lorem ipsum dolor sit amet.</h3>
-        <p className='my-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate autem sunt maiores obcaecati iste rerum.</p>
-        <p className='my-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate autem sunt maiores obcaecati iste rerum.</p>
+        <div className='blog-content' dangerouslySetInnerHTML={{__html:data.description}}>
 
-        <h3 className='my-5 text-[18px] font-semibold'>Step 2: Lorem ipsum dolor sit amet.</h3>
-        <p className='my-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate autem sunt maiores obcaecati iste rerum.</p>
-        <p className='my-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate autem sunt maiores obcaecati iste rerum.</p>
-
-        <h3 className='my-5 text-[18px] font-semibold'>Conclusion</h3>
-        <p className='my-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate autem sunt maiores obcaecati iste rerum.</p>
-
+        </div>
+       
         <div className='my-24'>
           <p className='text-black font-semibold my-4'>Share this article on social media</p>
           <div className='flex'>
