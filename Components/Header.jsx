876403be1c6243892d89adf4e4e9e2b91c6b9362
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-08-29 16:06:37 +0530

    email api  setup

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,8 +1,27 @@
 import { assets } from '@/Assets/assets'
+import axios from 'axios';
 import Image from 'next/image'
-import React from 'react'
-
+import React, { useState } from 'react'
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 const Header = () => {
+
+  const [email,setEmail]=useState("");
+
+  const onSubmithandler=async(e)=>{
+    e.preventDefault();
+    const formData=new FormData();
+    formData.append("email",email);
+    const response=await axios.post('/api/email',formData);
+    if(response.data.success){
+      toast.success(response.data.msg);
+
+    }
+    else{
+      toast.error("Error")
+    }
+  }
+
   return (
     <div className='py-5 px-5 md:px-12 lg:px-28'>
       <div className=' flex justify-between items-center'>
@@ -14,8 +33,15 @@ const Header = () => {
        <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blogs</h1>
        <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque vel pariatur, facilis dolorem ratione asperiores nostrum soluta nobis cumque quibusdam nemo laborum aut sint, sequi laboriosam amet illo iste sapiente. Pariatur inventore cupiditate deleniti ullam optio dolores ea consequatur ipsa?</p> 
 
-       <form action="" className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]'  >
-        <input type="email"  placeholder='Enter Your Email' className='pl-4 outline-none'/>
+       <form onSubmit={onSubmithandler} action="" className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]'  >
+       
+        <input 
+        onChange={(e)=>{setEmail(e.target.value)}}
+        value={email}
+        type="email"  
+        placeholder='Enter Your Email'
+         className='pl-4 outline-none'/>
+       
        <button className='border-l border-black py-4 sm:px-8 active:bg-gray-600 active:text-white' type='submit'>Subscribe</button>
        </form>
     </div>  
--- app/page.jsx
+++ app/page.jsx
@@ -3,10 +3,12 @@ import BlogItem from "@/Components/BlogItem";
 import BlogList from "@/Components/BlogList";
 import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
-
+import { ToastContainer } from "react-toastify";
 export default function Home() {
   return (
     <>
+
+    <ToastContainer theme="dark"/>
     <Header/>
     <BlogList/>
     <Footer/>
